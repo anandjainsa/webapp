558a17825563febb2f48414df031eeefc4474114
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-08-27 17:37:58 +0530

    Update appDeploy.groovy

--- vars/appDeploy.groovy
+++ vars/appDeploy.groovy
@@ -1,5 +1,8 @@
-def call(TOMCAT_IP,PORT,warPath)
+def call(TOMCAT_IP, PORT, warPath)
 {
-    sh "curl -u ifind:zs123456 http://${TOMCAT_IP}:${PORT}/manager/text/undeploy?path=${warPath}"
-    sh "curl --upload-file target/mvn-hello-world.war  -u ifind:zs123456 http://${TOMCAT_IP}:${PORT}/manager/text/deploy?path=${warPath}"
+    sh '''
+    curl -u ifind:zs123456 http://${TOMCAT_IP}:${PORT}/manager/text/undeploy?path=${warPath}
+    curl --upload-file target/mvn-hello-world.war  -u ifind:zs123456 http://${TOMCAT_IP}:${PORT}/manager/text/deploy?path=${warPath}
+    echo "Anand Jain ${TOMCAT_IP}:${PORT}"
+    '''
 }
