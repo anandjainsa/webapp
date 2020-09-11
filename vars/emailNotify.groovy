ca9f5e7774d73f442cca6cf83ee2b24e332b4441
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-09-11 14:13:55 +0530

    Create emailNotify.groovy

---
+++ vars/emailNotify.groovy
@@ -0,0 +1,8 @@
+def call(){
+                
+   emailext attachLog: true, attachmentsPattern: 'target/*.war',
+                body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
+                recipientProviders: [developers(), requestor()],
+                subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}"
+
+}
